39ae51b6137c3142212e7d327eaa7270d546793a
Sumit Singh <sumiterw11@gmail.com>
2019-09-07 17:07:10 +0530

    minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'home.apps.HomeConfig',
+    'events.apps.EventsConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
